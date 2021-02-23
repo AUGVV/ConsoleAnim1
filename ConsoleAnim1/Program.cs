3a2b776f7da124602269535680929c4a2398c791
Вячеслав Аугуль <Augul.V@tatbep.by>
2021-02-23 11:02:36 +0300

    v1

---
+++ ConsoleAnim1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            string result = "";
+            while (true)
+            {
+                Random rnd = new Random();
+                for (int i = 0; i < 78; i++)
+                {              
+                    int value = rnd.Next(0, 100);
+                    if (value < 1.5+30)
+                    {
+                        result += 1;
+                    }
+                    else
+                    {
+                        result += 0;
+                    }
+                }
+                Console.WriteLine(result);
+
+                Thread.Sleep(70);
+                result = "";
+            }
+        }
+    }
+}
